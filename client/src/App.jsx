26d74e1e0f512f097f12263a2bc20e8950ab6b1a
Himanshu Soni <himanshua790@gmail.com>
2024-03-22 18:42:11 +0530

    init setup

---
+++ client/src/App.jsx
@@ -0,0 +1,16 @@
+import "./App.css";
+import Canvas from "./canvas";
+import Home from "./pages/Home";
+import Customizer from "./pages/Customizer";
+
+function App() {
+  return (
+    <div className="app transition-all ease-in">
+      <Home />
+      <Canvas />
+      <Customizer />
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,5 @@
+const Homes = () => {
+  return <div>Hello</div>;
+};
+
+export default Homes;
